20efa805919151778712adada196c91e216db9ad
Joshua-Onley <jronley@btinternet.com>
2024-07-23 13:42:54 +0100

    Initial comit

--- alpha_beta_minimax.py
+++ alpha_beta_minimax.py
@@ -101,5 +101,3 @@ def alpha_beta_minimax(depth, maximizing_player, alpha, beta):
                 break
         transposition_table[board_hash] = (min_eval, depth)
         return min_eval, best_move
-
-'test'
